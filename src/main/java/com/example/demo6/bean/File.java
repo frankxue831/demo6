beea56670ea7b3afe14fe1e8c4c9e4834b9098b7
Fengxiang Xue <47923440+frankxue831@users.noreply.github.com>
2021-04-26 16:16:43 +0800

    adding new class File and industryStandardFile

---
+++ src/main/java/com/example/demo6/bean/File.java
@@ -0,0 +1,11 @@
+package com.example.demo6.bean;
+
+import lombok.Data;
+
+@Data
+public abstract class File {
+    public String id;
+    public String name;
+    public String seriesID;
+
+}
---
+++ src/main/java/com/example/demo6/bean/industryStandardFile.java
@@ -0,0 +1,8 @@
+package com.example.demo6.bean;
+
+import lombok.Data;
+
+@Data
+public class industryStandardFile extends File{
+    private String publishDate;
+}
